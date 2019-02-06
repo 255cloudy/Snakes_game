1f3bbd82b20ec2199aa2db1b55023e5af76a7df5
root <root@localhost.localdomain>
2019-02-06 23:16:30 +0300

    completed functions to control themovement of the head of the snake

--- base.py
+++ base.py
@@ -1,25 +1,28 @@
 import pygame, sys
+import random
 from pygame.locals import *
 
 sp_val = 1
 speed = [1, 1]
-width = 500
-height = 300
+width = 800
+height = 400
+food_color = [255, 100, 100]
 size = (width, height)
 up = [0, -sp_val]
 down = [0, sp_val]
 left = [-sp_val, 0]
 right = [sp_val, 0]
-black = (0, 0, 0)
+black = (100, 100, 100)
+direct = right
 # ball = pygame.image.load('ball.jpeg')
 # ball_rect = ball.get_rect()
 screen = pygame.display.set_mode(size)
 
 
-class Snake(pygame.sprite.Sprite):
+class Segment(pygame.sprite.Sprite):
 
     def __init__(self):
-        super(Snake, self).__init__()
+        super(Segment, self).__init__()
         self.surf = pygame.Surface((10, 10))
         self.rect = self.surf.get_rect()
         self.surf.fill((255, 255, 255))
@@ -27,48 +30,64 @@ class Snake(pygame.sprite.Sprite):
         self.left = self.rect.left
         self.top = self.rect.right
         self.bottom = self.rect.bottom
-        self.direction = right
+        self.direction = direct
 
-    # overriding the update() function  to update the sprites position
+    # overriding the update() function  to update the sprites direction
     def update(self, direction):
         self.direction = direction
+        self.move()
+        contsr(self)
+
+    # function changes the position of a segment of the snake
+    def move(self):
+        self.rect.x += self.direction[0]
+        self.rect.y += self.direction[1]
 
 
 class Food(pygame.sprite.Sprite):
 
     def __init__(self):
         super(Food, self).__init__()
-        self.surf = pygame.Surface(10, 10)
+        self.surf = pygame.Surface((10, 10))
+        self.surf.fill(food_color)
         self.rect = self.surf.get_rect()
+        self.rect.x = random.randint(1, width)
+        self.rect.y = random.randint(1, height)
 
 
 segs = pygame.sprite.Group()
+foods = pygame.sprite.Group()
 
 
 # this function will create a segment and assign its position on the snake
 def create_seg():
-    seg = Snake()
+    seg = Segment()
     segs.add(seg)
 
 
-# this function is supposed to update the position of all the segments
-def update_segs(direction):
-    for seg in segs:
-        seg.update(direction)
+# this function helps us create a food object
+def create_food():
+    food = Food()
+    foods.add(food)
 
 
-# this function ensures that the snake remains within the screen
-def constraint(obj):
-    if obj.right > width:
-        obj.right = 0
-    if obj.top > height:
-        obj.top = 0
+# function checks to ensure that the segments are within the window
+def contsr(seg):
+    if seg.rect.x > width:
+        seg.rect.x = 0
+    if seg.rect.x < 0:
+        seg.rect.x = width
+    if seg.rect.y > height:
+        seg.rect.y = 0
+    if seg.rect.y < 0:
+        seg.rect.y = height
 
 
 if __name__ == '__main__':
     # initialise the game
     pygame.init()
     create_seg()
+    food = Food()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -76,19 +95,20 @@ if __name__ == '__main__':
             # check for events then change the direction attribute for the snake
             if event.type == pygame.KEYDOWN:
                 if event.key == K_DOWN:
-                    update_segs(down)
+                    direct = down
                 if event.key == K_UP:
-                    update_segs(up)
+                    direct = up
                 if event.key == K_RIGHT:
-                    update_segs(right)
+                    direct = right
                 if event.key == K_LEFT:
-                    update_segs(left)
+                    direct = left
 
-            # erase the screen by rewriting with black color
-            screen.fill(black)
-            for seg in segs:
-                constraint(seg)
-                screen.blit(seg.surf, seg.rect)
-            pygame.display.flip()
+        segs.update(direct)
+        # erase the screen by rewriting with black color
+        screen.fill(black)
+        for seg in segs:
+            screen.blit(seg.surf, seg.rect)
+        screen.blit(food.surf, food.rect)
+        pygame.display.flip()
 
 
