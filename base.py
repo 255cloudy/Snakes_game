c2c01f7e401dec0a93055406134ceb4b8bf3c9b8
rucha <collorucha@gmail.com>
2019-02-07 17:24:22 +0300

    days changes

--- base.py
+++ base.py
@@ -1,4 +1,5 @@
-import pygame, sys
+import pygame
+import sys
 import random
 from pygame.locals import *
 
@@ -14,9 +15,15 @@ left = [-sp_val, 0]
 right = [sp_val, 0]
 black = (100, 100, 100)
 direct = right
+clock = pygame.time.Clock()
 # ball = pygame.image.load('ball.jpeg')
 # ball_rect = ball.get_rect()
 screen = pygame.display.set_mode(size)
+
+
+class Snake:
+    def __init__(self):
+        self.head = 0
 
 
 class Segment(pygame.sprite.Sprite):
@@ -26,6 +33,8 @@ class Segment(pygame.sprite.Sprite):
         self.surf = pygame.Surface((10, 10))
         self.rect = self.surf.get_rect()
         self.surf.fill((255, 255, 255))
+        self.x = self.rect.x
+        self.y = self.rect.y
         self.right = self.rect.right
         self.left = self.rect.left
         self.top = self.rect.right
@@ -56,19 +65,31 @@ class Food(pygame.sprite.Sprite):
 
 
 segs = pygame.sprite.Group()
-foods = pygame.sprite.Group()
-
+snake = Snake()
 
 # this function will create a segment and assign its position on the snake
 def create_seg():
     seg = Segment()
+    segs_len = len(segs)
+    if segs_len == 0:
+        snake.head = seg
+    # this is the logic that determines where to place the next segment incase the snake eats food
+    elif segs_len > 0:
+        if direct == down:
+            seg.y = snake.head.y + (segs_len)
+        if direct == up:
+            seg.y = snake.head.y - (segs_len)
+        if direct == right:
+            seg.x = snake.head.x - (segs_len)
+        if direct == left:
+            seg.x = snake.head.y+(segs_len)
     segs.add(seg)
 
 
 # this function helps us create a food object
 def create_food():
     food = Food()
-    foods.add(food)
+    return food
 
 
 # function checks to ensure that the segments are within the window
@@ -102,13 +123,18 @@ if __name__ == '__main__':
                     direct = right
                 if event.key == K_LEFT:
                     direct = left
-
         segs.update(direct)
         # erase the screen by rewriting with black color
+        hit = pygame.sprite.collide_rect(food, snake.head)
+        if hit:
+            create_seg()
+            food.kill()
+            food = create_food()
         screen.fill(black)
         for seg in segs:
             screen.blit(seg.surf, seg.rect)
         screen.blit(food.surf, food.rect)
         pygame.display.flip()
+        clock.tick(100)
 
 
